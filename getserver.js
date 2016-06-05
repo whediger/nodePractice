1fa2b13e7f18562527e32aca8dd943deeb09f3aa
Wes Hediger <wes.hediger@gmail.com>
2016-06-04 23:09:19 -0600

    remove is-number-prime module

--- getserver.js
+++ getserver.js
@@ -1,3 +1,6 @@
+
+
+
 var http = require('http');
 var url = require('url');
 var qstring = require('querystring');
@@ -6,12 +9,16 @@ http.createServer(function(req, res){
   console.log(req.url);
   var stuff = qstring.parse(req.url);
 
-  console.log(stuff.hello);
+  console.log(stuff.number);
   res.setHeader('Content-Type', 'text/html');
   res.writeHead(200);
   res.write('<html><head><title>Simple GET Server</title></head>');
   res.write('<body>');
-  res.write('<h2>did you say ' + stuff.hello + '?')
+  res.write('<h2>did you say ' + stuff.number + '?');
+
+
+
+  res.write('<h2>the number ' + stuff.number + ' is ');
   res.write('<h1>Counting from 0 to 100');
   for ( i = 0; i < 101; i++ ){
   res.write('<p>' + i + '</p>');
