824d0f834f0005026db6de06e8b1bad9e0c3acd8
Wes Hediger <wes.hediger@gmail.com>
2016-06-04 20:40:57 -0600

    add query to output on screen

--- getserver.js
+++ getserver.js
@@ -6,14 +6,15 @@ http.createServer(function(req, res){
   console.log(req.url);
   var stuff = qstring.parse(req.url);
 
-  console.log(stuff);
+  console.log(stuff.hello);
   res.setHeader('Content-Type', 'text/html');
   res.writeHead(200);
   res.write('<html><head><title>Simple GET Server</title></head>');
   res.write('<body>');
+  res.write('<h2>did you say ' + stuff.hello + '?')
   res.write('<h1>Counting from 0 to 100');
   for ( i = 0; i < 101; i++ ){
-    res.write('<p>' + i + '</p>');
+  res.write('<p>' + i + '</p>');
   }
   res.end('\n</body></html>');
 }).listen(8080);
