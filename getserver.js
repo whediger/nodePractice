01ee66230428525bdfd7c52900461a0c50fa4998
Wes Hediger <wes.hediger@gmail.com>
2016-06-05 00:10:55 -0600

    add previous and next prime functions

--- getserver.js
+++ getserver.js
@@ -1,25 +1,39 @@
 
 var Nprime = require('nprime');
+//see https://www.npmjs.com/package/nprime
+//for method deffinitions
+
 
 var http = require('http');
 var url = require('url');
 var qstring = require('querystring');
+//format for query +=={=======> http://localhost:8080/&number=12
 
 http.createServer(function(req, res){
   console.log(req.url);
   var stuff = qstring.parse(req.url);
-
-  console.log(stuff.number);
+  var numberIn = stuff.number;
+  console.log(numberIn);
   res.setHeader('Content-Type', 'text/html');
   res.writeHead(200);
   res.write('<html><head><title>Simple GET Server</title></head>');
   res.write('<body>');
-  res.write('<h2>did you say ' + stuff.number + '?');
+  res.write('<h2>did you say ' + numberIn + '?');
 
-  var result = Nprime.test(7);
+  var result = Nprime.test(numberIn);
+  var prevPrime = Nprime.prev(numberIn)
+  var nextPrime = Nprime.next(numberIn);
   console.log(result);
+  console.log(nextPrime);
 
-  res.write('<h2>the number ' + stuff.number + ' is ');
+  if ( result === -1 ) {
+    result = "a prime"
+  } else {
+    result = "not a prime"
+  }
+  res.write('<h2>the number ' + numberIn + ' is ' + result + '</h2>');
+  res.write('<h2>the previous prime was ' + prevPrime + '</h2>');
+  res.write('<h2>the next prime is ' + nextPrime + '</h2>');
   res.write('<h1>Counting from 0 to 100');
   for ( i = 0; i < 101; i++ ){
   res.write('<p>' + i + '</p>');
