2a15d227de185e5ac88357498a621c18a218ca9e
Wes Hediger <wes.hediger@gmail.com>
2016-06-04 18:46:35 -0600

    add get server

---
+++ getserver.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+
+http.createServer(function(req, res){
+  res.setHeader('Content-Type', 'text/html');
+  res.writeHead(200);
+  res.write('<html><head><title>Simple GET Server</title></head>');
+  res.write('<body>');
+  res.write('<h1>Counting from 0 to 100');
+  for ( i = 0; i < 101; i++ ){
+    res.write('<p>' + i + '</p>');
+  }
+  res.end('\n</body></html>');
+}).listen(8080);
